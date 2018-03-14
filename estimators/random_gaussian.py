0374cf59e133920c4ea2e01ae15ae8aa84362c32
loeiten <michael.l.magnussen@gmail.com>
2018-03-14 22:54:28 +0100

    Added estimator skeleton

---
+++ estimators/random_gaussian.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class RandomGaussian(object):
+    """
+    FIXME
+    """
+
+    def __init__(self):
+        """
+        FIXME.
+        """
+
+    def fit(self, x, y):
+        """
+        Detects the soft boundary of the set of samples x.
+
+        Parameters
+        ----------
+        x : array-like, shape (n_samples, n_features)
+            Set of samples, where n_samples is the number of samples and
+            n_features is the number of features.
+        y : array-like, shape n_samples
+            Set of corresponding labels to the samples.
+        """
+
+        # FIXME.
+
+    def predict(self, x):
+        """
+        Perform classification on samples in x.
+
+        Parameters
+        ----------
+        x : array-like, shape (n_samples, n_features)
+            Set to perform classification on.
+
+        Returns
+        -------
+        y_pred : array, shape (n_samples,)
+            Class labels for samples in X.
+        """
+
+        # FIXME
