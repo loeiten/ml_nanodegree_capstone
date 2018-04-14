0d6c6818466f81c1387a87904465e59892cec747
loeiten <michael.l.magnussen@gmail.com>
2018-04-14 17:50:10 +0200

    Added MaxMin and test

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -4,6 +4,7 @@
 import unittest
 import pandas as pd
 import numpy as np
+from utils.transformations import StockMinMax
 from utils.transformations import subtract_previous_row
 
 
@@ -26,6 +27,52 @@ class TestTransformations(unittest.TestCase):
         self.assertTrue(np.allclose(result.values,
                                     expected.values,
                                     equal_nan=True))
+
+
+class TestStockMinMax(unittest.TestCase):
+    def setUp(self):
+        self.df = pd.DataFrame(np.array([[1, 2, 3, 4],
+                                         [5, 6, 7, 8],
+                                         [9, 10, 11, 12]]),
+                               columns=['a', 'a + 1', 'c', 'c - 1'],
+                               dtype=float)
+
+    def test_fit(self):
+        scaler = StockMinMax()
+        scaler.fit(self.df)
+        clean_features = ['a', 'c']
+
+        max_scaler =\
+            pd.Series(self.df.loc[:, clean_features].values.max(axis=0),
+                      index=['a', 'c'])
+        self.assertTrue(scaler.max.equals(max_scaler))
+
+        min_scaler =\
+            pd.Series(self.df.loc[:, clean_features].values.min(axis=0),
+                      index=['a', 'c'])
+        self.assertTrue(scaler.min.equals(min_scaler))
+
+    def test_transform(self):
+        scaler = StockMinMax()
+        scaler.fit(self.df)
+
+        expected = pd.DataFrame(np.array([[0, 0.125, 0, 0.125],
+                                          [0.5, 0.625, 0.5, 0.625],
+                                          [1, 1.125, 1, 1.125]]),
+                                columns=['a', 'a + 1', 'c', 'c - 1'],
+                                dtype=float)
+
+        x_prime = scaler.transform(self.df)
+
+        self.assertTrue(x_prime.equals(expected))
+
+    def test_inverse_transform(self):
+        scaler = StockMinMax()
+        scaler.fit(self.df)
+
+        x = scaler.inverse_transform(scaler.transform(self.df))
+
+        self.assertTrue(x.equals(self.df))
 
 
 if __name__ == '__main__':
--- utils/transformations.py
+++ utils/transformations.py
@@ -2,6 +2,119 @@
 # -*- coding: utf-8 -*-
 
 
+class StockMinMax(object):
+    """
+    MinMax scaler for stocks.
+
+    Attributes
+    ----------
+    max : pandas.core.series.Series
+        The maximum value of the clean features (i.e. without - or + in the
+        names), neglecting nans
+    min : pandas.core.series.Series
+        The minimum value of the clean features (i.e. without - or + in the
+        names), neglecting nans
+
+    Examples
+    --------
+    FIXME
+
+    """
+
+    def __init__(self):
+        """
+        Declares the member variables.
+        """
+
+        self.max = None
+        self.min = None
+
+    def fit(self, x):
+        """
+        Fit the variables of the scaler.
+
+        Only 'clean' features will be taken into account.
+        I.e. if x contains the features 'foo', 'foo - 1', 'bar', only 'foo'
+        and 'bar' will be fitted.
+
+        Parameters
+        ----------
+        x : DataFrame
+            The fitting data
+        """
+
+        clean_cols = [col for col in x.columns
+                      if '-' not in col and '+' not in col]
+
+        self.max = x.loc[:, clean_cols].max(skipna=True)
+        self.min = x.loc[:, clean_cols].min(skipna=True)
+
+    def transform(self, x):
+        """
+        Transforms x.
+
+        Features will be scaled according to the features in self.max and
+        self.min by
+
+        x' = (x - x_min)/(x_max - x_min)
+
+        Parameters
+        ----------
+        x : DataFrame
+            The data frame to transform.
+
+        Returns
+        -------
+        x_prime : DataFrame
+            The transformed data.
+        """
+
+        x_prime = x.copy()
+
+        x_cols = x.columns
+        for max_col in self.max.index:
+            for x_col in x_cols:
+                if max_col == x_col[:len(max_col)]:
+                    x_prime.loc[:, x_col] =\
+                        (x.loc[:, x_col] - self.min.loc[max_col]) / \
+                        (self.max.loc[max_col] - self.min.loc[max_col])
+
+        return x_prime
+
+    def inverse_transform(self, x_prime):
+        """
+        Inverse transforms x.
+
+        Features will be scaled according to the features in self.max and
+        self.min by
+
+         x = x'*(x_max - x_min) + x_min
+
+        Parameters
+        ----------
+        x_prime : DataFrame
+            The data frame to transform.
+
+        Returns
+        -------
+        x : DataFrame
+            The transformed data.
+        """
+
+        x = x_prime.copy()
+
+        x_cols = x_prime.columns
+        for max_col in self.max.index:
+            for x_col in x_cols:
+                if max_col == x_col[:len(max_col)]:
+                    x.loc[:, x_col] = \
+                        x_prime.loc[:, x_col] * \
+                        (self.max.loc[max_col] - self.min.loc[max_col])\
+                        + self.min.loc[max_col]
+
+        return x
+
+
 def subtract_previous_row(df, cols=None, copy=False):
     """
     Subtract the previous row with the current row.
