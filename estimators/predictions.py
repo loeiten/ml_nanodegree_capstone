0eb9b7a073536f40303605298f5349a1bf85a55c
loeiten <michael.l.magnussen@gmail.com>
2018-04-15 10:17:40 +0200

    Added possibility to have a training prediction

--- estimators/predictions.py
+++ estimators/predictions.py
@@ -7,7 +7,13 @@ import numpy as np
 from sklearn.multioutput import MultiOutputRegressor
 
 
-def calculate_rolling_prediction(reg, x_train, x_test, y_train, y_test):
+def calculate_rolling_prediction(reg,
+                                 x_train,
+                                 x_test,
+                                 y_train,
+                                 y_test,
+                                 prediction_days,
+                                 training_prediction=False):
     """
     Returns a rolling prediction.
 
@@ -42,11 +48,20 @@ def calculate_rolling_prediction(reg, x_train, x_test, y_train, y_test):
         The targets of the training set
     y_test :  DataFrame, shape (n_test_samples, n_targets)
         The targets of the test set
+    prediction_days : array-like, shape (n_targets)
+        The number of days the targets aim to predict
+    training_prediction : bool
+        Whether or not predictions should be made on the training set
 
     Returns
     -------
-    pred_df : DataFrame
+    pred_df : DataFrame, shape (n_test_samples - min_nans, n_targets)
         The DataFrame containing the predictions.
+        The minimum amount of trailing NaNs will be stripped from the end.
+    train_pred_df : DataFrame, shape (n_test_samples - min_nans, n_targets)
+        The DataFrame containing the predictions on the fitted training set.
+        Is shifted one index compared to the pred_df.
+        The minimum amount of trailing NaNs will be stripped from the end.
 
     Examples
     --------
@@ -84,12 +99,16 @@ def calculate_rolling_prediction(reg, x_train, x_test, y_train, y_test):
     """
 
     # Initialize the DataFrames list
-    df_list = []
+    pred_list = list()
 
-    # Obtain the day of prediction
-    # I.e. for a column named x + 2 days, we would expect the two last rows
-    # to contain nan
-    prediction_days = y_test.isnull().sum()
+    if training_prediction:
+        train_pred_list = list()
+
+    # If feature_generator has been used, then NaNs have been
+    # introduced in the training set. We remove these
+    introduced_nans = x_train.isnull().sum().max()
+    x_train = x_train.iloc[introduced_nans:]
+    y_train = y_train.iloc[introduced_nans:]
 
     # Do the rolling prediction for each of the columns in y
     for col_ind in range(len(y_test.columns)):
@@ -106,6 +125,9 @@ def calculate_rolling_prediction(reg, x_train, x_test, y_train, y_test):
         # due to the shift)
         y_pred = np.empty(y_test_cur_col.shape[0] - days)
 
+        if training_prediction:
+            y_train_pred = np.empty(y_test_cur_col.shape[0] - days)
+
         for pred_nr in range(len(y_pred)):
             # Extend the training data
             # NOTE: x_test.iloc[:0] will return an empty DataFrame
@@ -114,26 +136,49 @@ def calculate_rolling_prediction(reg, x_train, x_test, y_train, y_test):
             rolling_y = pd.concat([y_train_cur_col,
                                    y_test_cur_col.iloc[:pred_nr]],
                                   axis=0)
+
             # Fit (retrain) the model with the rolling_features set
             reg.fit(rolling_x.values, rolling_y.values)
             # Make prediction and append to y_pred
             # Predict for test value after the test value which was just
             # included to rolling_x
             # NOTE: There are as many predictions as the length of y_pred.
-            #       The first prediction is using data only from the test set.
+            #       The first prediction is using data only from the training
+            #       set.
             #       x_test.iloc[0] returns the first element
             y_pred[pred_nr] = \
                 reg.predict(x_test.iloc[pred_nr].values[np.newaxis, :])[-1]
+
+            if training_prediction:
+                # NOTE: When fitting, we use the data until (but no
+                #       including) the pred_nr index of x_test.
+                #       y_pred_train is making a prediction on this element,
+                #       whereas y_pred is making a prediction on the next
+                #       element
+                y_train_pred[pred_nr] = \
+                    reg.predict(rolling_x.iloc[-1].values[np.newaxis, :])[-1]
 
         # Cast the result into a DataFrame for easier post-processing
         # The indexing from y_test includes the first days where there will
         # be no prediction for (except the one done from the pure training
         # set), and the nan values at the end of y_test due to the shift
-        df_list.append(pd.DataFrame(y_pred,
-                       index=y_test_cur_col.index[: -days],
-                       columns=[y_test_cur_col.name + ' predicted']))
+        pred_list.append(pd.DataFrame(y_pred,
+                         index=y_test_cur_col.index[: -days],
+                         columns=[y_test_cur_col.name + ' predicted']))
 
-    pred_df = pd.concat(df_list, axis=1)
+        if training_prediction:
+            index = list([y_train_cur_col.index[-1],
+                          *y_test_cur_col.index[: -days - 1]])
+            train_pred_list.append(
+                pd.DataFrame(y_train_pred,
+                             index=index,
+                             columns=[y_test_cur_col.name + ' fit prediction']))
+
+    pred_df = pd.concat(pred_list, axis=1)
+    if training_prediction:
+        train_pred_df = pd.concat(train_pred_list, axis=1)
+
+        return pred_df, train_pred_df
 
     return pred_df
 
--- tests/test_predictions.py
+++ tests/test_predictions.py
@@ -20,6 +20,11 @@ class TestPredictions(unittest.TestCase):
         self.x_train, self.x_test, self.y_train, self.y_test = \
             train_test_split(x, y, shuffle=False, test_size=12)
 
+        # Obtain the day of prediction
+        # I.e. for a column named x + 2 days, we would expect the two last rows
+        # to contain nan
+        self.prediction_days = self.y_test.isnull().sum()
+
     def test_calculate_rolling_prediction(self):
         expected = pd.DataFrame(np.array(
             [[6., 7.],
@@ -42,10 +47,42 @@ class TestPredictions(unittest.TestCase):
                                          self.x_train,
                                          self.x_test,
                                          self.y_train,
-                                         self.y_test)
+                                         self.y_test,
+                                         self.prediction_days)
 
         self.assertTrue(np.allclose(result.values,
                                     expected.values,
+                                    equal_nan=True))
+
+        result, train_result = \
+            calculate_rolling_prediction(reg,
+                                         self.x_train,
+                                         self.x_test,
+                                         self.y_train,
+                                         self.y_test,
+                                         self.prediction_days,
+                                         training_prediction=True)
+
+        self.assertTrue(np.allclose(result.values,
+                                    expected.values,
+                                    equal_nan=True))
+
+        train_expected = pd.DataFrame(np.array(
+            [[5., 6.],
+             [6., 7.],
+             [7., 8.],
+             [8., 9.],
+             [9., 10.],
+             [10., 11.],
+             [11., 12.],
+             [12., 13.],
+             [13., 14.],
+             [14., np.nan]]),
+            columns=['x + 2 days fit prediction', 'x + 3 days fit prediction'],
+            index=range(3, 13))
+
+        self.assertTrue(np.allclose(train_result.values,
+                                    train_expected.values,
                                     equal_nan=True))
 
     def test_calculate_normal_prediction(self):
@@ -91,7 +128,8 @@ class TestPredictions(unittest.TestCase):
                                          self.x_train,
                                          self.x_test,
                                          self.y_train,
-                                         self.y_test)
+                                         self.y_test,
+                                         self.prediction_days)
 
         self.assertTrue(np.allclose(result.values,
                                     expected.values,
